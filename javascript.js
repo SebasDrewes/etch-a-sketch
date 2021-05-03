22a771899bfe88868f6f7ee3ba6f0d9edae2e783
Sebastian Drewes <sebas.drewes@gmail.com>
2021-05-02 23:51:35 -0300

    updated

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
 const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
-let cell = document.getElementsByClassName("cell");
+let cells = document.getElementsByClassName("cell");
 const customGrid = document.getElementById('customGrid');
-
 
 defaultGrid(16);
 
@@ -15,6 +14,10 @@ function defaultGrid(squares) {
     else {
     makeRows(squares);
     makeColumns(squares);
+    for (let i = 0; i < cells.length; i++){
+        cells[i].addEventListener("mouseover", () => {
+        cells[i].style.background = "black";
+        })};
     }
 }
 function makeRows(rowNum) {
@@ -47,9 +50,37 @@ function removeAllChildNodes(parent) {
 customGrid.addEventListener('click', () => {
     removeAllChildNodes(container)
     defaultGrid(prompt("Cuadrados por lado (no mas de 64)", 16))
-});
+    for (let i = 0; i < cells.length; i++){
+        cells[i].addEventListener("mouseover", () => {
+        cells[i].style.background = "black";
+})}});
+//randomColor.addEventListener('click', () => {
+//   newColor = getRandomColor();
+//    for(let i = 0; i < cell.length; ++i) {
+//    cell[i].style.background = newColor;
+//}});
 randomColor.addEventListener('click', () => {
-    newColor = getRandomColor();
-    for(let i = 0; i < cell.length; ++i) {
-    cell[i].style.background = newColor;
-}});
+newColor = getRandomColor();
+for (let i = 0; i < cells.length; i++){
+cells[i].addEventListener("mouseover", () => {
+cells[i].style.background = newColor;
+})}});
+
+rainbow.addEventListener('click', () => {
+for (let i = 0; i < cells.length; i++){
+cells[i].addEventListener("mouseover", () => {
+rainbowColor = getRandomColor();
+cells[i].style.background = rainbowColor;
+})}});
+
+black.addEventListener('click', () => {
+for (let i = 0; i < cells.length; i++){
+cells[i].addEventListener("mouseover", () => {
+cells[i].style.background = "black";
+})}});
+
+borrador.addEventListener('click', () => {
+for (let i = 0; i < cells.length; i++){
+cells[i].addEventListener("mouseover", () => {
+cells[i].style.background = "rgb(235, 231, 231)";
+})}});
